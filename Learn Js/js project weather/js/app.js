af54800f9faa81ef4f7d21a0f076909f019e32c1
skdev6 <sukhan471@gmail.com>
2020-12-20 20:57:08 +0600

    first

---
+++ Learn Js/js project weather/js/app.js
@@ -0,0 +1,15 @@
+const condition = document.getElementById('condition');
+const city = document.getElementById('city');
+const country = document.getElementById('country');
+const mainText = document.getElementById('main');
+const description = document.getElementById('description');
+const temp = document.getElementById('temp');
+const pressure = document.getElementById('pressure');
+const humidity = document.getElementById('humidity');
+
+const cityInput = document.getElementById('city-input');
+const historyElm = document.getElementById('history');
+const masterHistory = document.getElementById('master-history');
+
+
+
